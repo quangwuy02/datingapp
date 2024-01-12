9fdd1ea0b599a725d06080c8cda885baa3955923
quangwuy02 <huyqlht@gmail.com>
2024-01-12 16:10:06 +0700

    update

--- backend/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ backend/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1e53d559594b487f01cc24a5712d5b6a1b7704e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ba08ceadb6b5dcf6040696537b41ae5cf9cc0b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
